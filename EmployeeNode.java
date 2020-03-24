8d78e9e682037b8942801ab41ef8adb7f980fb61
Elena Voinu <37947819+Lenka111@users.noreply.github.com>
2020-03-23 22:19:41 -0500

    Update EmployeeNode.java

--- EmployeeNode.java
+++ EmployeeNode.java
@@ -1,8 +1,11 @@
 //Elena Voinu
-//Node class. Specific to the type we're using it with. Will only work with employee instances(not generic)
+//Node class. Specific to the type we're using it with. 
+//Will only work with employee instances(not generic)
 public class EmployeeNode {
+    
     //field for employee
     private  Employee employee;
+    
     // stores the next field in the list
     private  EmployeeNode next;
 
@@ -11,11 +14,10 @@ public class EmployeeNode {
         return employee.toString();
     }
 
-    //constructor only takes employee because
-    // when we construct an instance we don't know yet what the next node will be
-    //node is the element in the list
-    //if a node is the last node in the list then its next field will be set to null
-    //We don't have to set next to null in the constructor  because that is the default value for object fields
+    /*constructor only takes employee because when we construct an instance we don't know yet what the next node will be
+    node is the element in the list
+    if a node is the last node in the list then its next field will be set to null
+    We don't have to set next to null in the constructor  because that is the default value for object fields*/
     public EmployeeNode(Employee employee) {
         this.employee = employee;
     }
